6c8330d2b0d8130dbbf9e043a982ec3f60865f00
Pietro <45976792+05e@users.noreply.github.com>
2022-07-22 13:36:58 +0000

    Update and rename install-dotfiles.sh to install-apps.sh

---
+++ install-apps.sh
@@ -0,0 +1,1 @@
+brew install librewolf keepassxc cyberduck spotify visual-studio-code discord transmission notion firefox && brew autoremove && brew cleanup
--- install-dotfiles.sh
+++
@@ -1,3 +0,0 @@
-brew install librewolf keepassxc cyberduck spotify visual-studio-code discord transmission notion firefox && brew autoremove && brew cleanup
-mkdir ~/.zsh && curl --http1.1 "https://raw.githubusercontent.com/05e/dotfiles/main/completion.zsh" --output "~/.zsh/completion.zsh"
-curl --http1.1 "https://raw.githubusercontent.com/05e/dotfiles/main/zshrc" --output "~/.zshrc"
