bb4186dea1bc2eb0bb20db8208a374811487099a
Pietro <45976792+05e@users.noreply.github.com>
2022-07-23 22:38:48 +0200

    Update config.def.h

--- arch/dwm/config.def.h
+++ arch/dwm/config.def.h
@@ -1,7 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
+static const char selbordercolor[]  = "#005577";
+static const char selbgcolor[]      = "#005577";
+static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 8;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
