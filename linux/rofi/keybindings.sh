e2f21aa0385c2857198492037aeb37688dec7c27
Pietro Bonaldo Gregori <pietro.bogre@gmail.com>
2023-06-28 23:16:20 +0200

    basic rofi setup

--- linux/rofi/keybindings.sh
+++ linux/rofi/keybindings.sh
@@ -2,5 +2,5 @@
 
 awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc |
     column -t -s $'\t' |
-    rofi -dmenu -i -p "keybindings:" -markup-rows -no-show-icons -config "~/.config/rofi/keybindings.rasi"
+    rofi -dmenu -i -p "keybindings" -markup-rows -no-show-icons -config "~/.config/rofi/keybindings.rasi"
 
