dce769ce3d5944740e4395f4e211bd501f0f86a4
Pietro Bonaldo Gregori <pietro.bogre@gmail.com>
2023-06-29 22:02:44 +0200

    prepend icon based on playing status

--- linux/polybar/scripts/scroll_music_status.sh
+++ linux/polybar/scripts/scroll_music_status.sh
@@ -2,10 +2,10 @@
 
 # see man zscroll for documentation of the following parameters
 zscroll -l 23 \
-        --delay 0.1 \
+        --delay 0.2 \
         --scroll-padding " | " \
         --match-command "`dirname $0`/get_music_status.sh --status" \
-        --match-text "Playing" "--scroll 1" \
-        --match-text "Paused" "--scroll 1" \
+        --match-text "Playing" "--before-text '󰝚 ' --scroll 1" \
+        --match-text "Paused" "--before-text '󰝛 ' --scroll 0" \
         --update-check true "`dirname $0`/get_music_status.sh" &
 wait
