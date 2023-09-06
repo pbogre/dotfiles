94cfde3be82ce43fa3a2c6dfeb0837c7762485e1
Pietro Bonaldo <pietro.bogre@gmail.com>
2023-09-06 20:27:40 +0200

    switch to lua for nvim config

---
+++ macos/nvim/lua/options.lua
@@ -0,0 +1,84 @@
+-- Misc
+vim.o.ignorecase   = true
+vim.wo.wrap        = false
+vim.o.encoding     = 'utf-8'
+vim.o.conceallevel = 1
+
+-- Search Highlight
+vim.o.showmatch = true
+vim.o.hlsearch  = true
+vim.o.incsearch = true
+
+-- Indentation
+vim.o.shiftwidth = 2
+vim.o.autoindent = true
+vim.o.backspace  = 'start,eol,indent'
+vim.cmd("filetype indent on")
+
+-- Line Numbers
+vim.o.number          = true
+vim.wo.relativenumber = true
+vim.o.cursorline      = true
+vim.o.ruler           = true
+
+-- Show Syntax
+vim.cmd("syntax on")
+
+-- Mouse Access
+vim.o.mouse = 'a'
+
+-- Clipboard
+vim.o.clipboard = 'unnamedplus'
+
+-- Cursor Type
+vim.o.guicursor = 'n-c-i:block'
+
+-- Remappings
+local options = { noremap = true, silent = true }
+
+vim.keymap.set("i", "\"",    "\"\"<left>",   options)
+vim.keymap.set("i", "\"\"",  "\"\"",         options)
+vim.keymap.set("i", "'",     "''<left>",     options)
+vim.keymap.set("i", "''",    "''",           options)
+vim.keymap.set("i", "(",     "()<left>",     options)
+vim.keymap.set("i", "()",    "()",           options)
+vim.keymap.set("i", "[",     "[]<left>",     options)
+vim.keymap.set("i", "[]",    "[]",           options)
+vim.keymap.set("i", "{",     "{}<left>",     options)
+vim.keymap.set("i", "{}",    "{}",           options)
+vim.keymap.set("i", "{<CR>", "{<CR>}<ESC>O", options)
+
+vim.keymap.set("n", "rs",    ":%s@search@replace@gc", options)
+vim.keymap.set("n", "<C-h>", "<cmd>:noh<CR>",         options)
+
+-- Plugins
+vim.cmd("filetype plugin on")
+
+-- Autocomplete
+local coc_options = { silent = true, noremap = true, expr = true, replace_keycodes = false }
+vim.keymap.set("i", "<TAB>",   'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', coc_options)
+vim.keymap.set("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], coc_options)
+vim.keymap.set("i", "<cr>",    [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], coc_options)
+
+-- Gitsigns
+require('gitsigns').setup({
+  signs = {
+    add          = { text = "|" },
+    change       = { text = "|" },
+    delete       = { text = "|" },
+    topdelete    = { text = "|" },
+    changedelete = { text = "|" },
+    untracked    = { text = "|" },
+  },
+})
+
+-- Telescope
+vim.keymap.set("n", "ff", ":Telescope find_files<CR>", options)
+vim.keymap.set("n", "fg", ":Telescope live_grep<CR>",  options)
+vim.keymap.set("n", "fb", ":Telescope buffers<CR>",    options)
+vim.keymap.set("n", "fs", ":Telescope git_status<CR>", options)
+
+-- Colorscheme
+vim.o.termguicolors = true
+vim.o.background    = 'light'
+vim.cmd("colorscheme everforest")
---
+++ macos/nvim/lua/plugins/init.lua
@@ -0,0 +1,18 @@
+return require("packer").startup(function(use)
+  use "wbthomason/packer.nvim"
+  use "sainnhe/everforest"
+  use "lewis6991/gitsigns.nvim"
+  use {'neoclide/coc.nvim', branch = 'release'}
+  use{
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.2",
+    requires = { {"nvim-lua/plenary.nvim"} }
+  }
+  use{
+    "nvim-treesitter/nvim-treesitter",
+    run = function()
+      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+      ts_update()
+    end
+  }
+end)
